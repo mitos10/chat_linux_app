b1e7505addb1a6c4818c0693252209442a734faf
mitos10 <alvaro1estan@gmail.com>
2021-11-30 01:12:42 +0100

    Integrated AES-128

--- lib/aes.h
+++ lib/aes.h
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <arpa/inet.h>
 
 #define AES_128 4
 #define AES_192 6
 #define AES_256 8
 
+char* aes_encrypt(char* string, char* pass, int mode);
+char* aes_decrypt(char* string, char* pass, int mode);
+
 char* _round_keys(char* key, int mode);
+void _round_key_matrix(char key[4][4], char* keys, int init_word);
+void _round_key_matrix2(char key[4][4], char* keys, int init_word);
+void _add_round_key(char block[4][4], char key[4][4]);
+void _shift_rows(char block[4][4], int inv);
+void _sub_bytes(char block[4][4], int inv);
+void _mix_columns(char block[4][4], int inv);
+char _cross_product(char block_byte, char matrix_byte);
--- testmodules/aes_module/aes_test.c
+++ testmodules/aes_module/aes_test.c
@@ -1,5 +1,8 @@
 #include "../../lib/aes.h"
 
 int main(){
-    _round_keys( "Thats my Kung Fu", AES_128);
+
+    char* aux = aes_encrypt( "Thats my Kung Fu", "Two One Nine TwoTwo One Nine TwoCosasne threeTwo", AES_128);
+    aes_decrypt( "Thats my Kung Fu", aux, AES_128);
+    
 }
